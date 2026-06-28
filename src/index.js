5919b9654644d3a0a77fb24c99c80e4698fb91bf
shobiianis <shoaibwork2000@gmail.com>
2026-06-28 18:37:04 +0500

    Much better code for connection Db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT,()=>{
+    console.log("Database connected and server is on")
+  })
+}).catch((err)=>{|
+  console.log("Mongodb connection failed on server",err)
+})

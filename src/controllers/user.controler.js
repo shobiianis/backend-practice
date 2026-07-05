e90a5c59a64022dd326148fb4e75aa9417e8920b
shobiianis <shoaibwork2000@gmail.com>
2026-07-05 19:14:25 +0500

    control function much better with validatiion

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -1,9 +1,87 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Shoaib anis khairani",
+  // req.body
+  // res.status(200).json({
+  //   data: {
+  //     key: "value",
+  //   },
+  //   message: "any message",
+  // });
+
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+
+  const { fullName, email, username, password } = req.body;
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+  /*
+  req.files eslye kyu k humne multer middleware use kia hai to ye req.files provide karta hai
+  jisme se hum avatar aur coverImage ka path nikal rahe hain 
+  */
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // yahan validation k baad ik user create horaha hai
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  /*  ab question is how can User can find the user so the answer is k ye USer jo schema model
+    se aya hai us k pass poora database
+    .select ye karta hai k by default all fields selected hoti hain 
+    but - password -refreshToken ye dono fields ko select nahi karega
+    */
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export { registerUser };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,27 @@ import { registerUser } from "../controllers/user.controler.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
+/*
+jab hum post ka keyword use karte hain to woh hume req,res,next provide karta hai
+aur phir wohi hum us controller k function k ander use karte hain 
+*/
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  /* upload function k ander fields ka use kiya hai 
+     jisme hum multiple image fields upload karsakte hain with masx count  1
+     */
+
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
 
 export default router;
